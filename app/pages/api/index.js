bbfd9d9146bc3c928e383fe040ea04daf12358d0
frinzekt <frinzekt@gmail.com>
2020-03-18 19:11:46 +0800

    api routes

---
+++ app/pages/api/index.js
@@ -0,0 +1,13 @@
+import middleware from '../../middleware/index';
+const users = [
+	{
+		id: 1
+	},
+	{ id: 2 },
+	{ id: 3 }
+];
+
+export default (req, res) => {
+	// Get data from your database
+	res.status(200).json(users);
+};

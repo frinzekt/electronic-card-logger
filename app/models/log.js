080278e152b37194126106da558e57687fb693e3
frinzekt <frinzekt@gmail.com>
2020-03-18 19:12:06 +0800

    logs model

---
+++ app/models/log.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const logSchema = mongoose.Schema(
+	{
+		deviceName: {
+			type: String,
+			required: true
+		},
+		type: {
+			type: String
+		}
+	},
+	{ collection: 'ECL_logs', timestamps: { createdAt: 'datetime' } }
+);
+
+module.exports = mongoose.models.Log || mongoose.model('Log', logSchema);

bcfae9c34dc5508f660b164b642d0d9c46353ad7
frinzekt <frinzekt@gmail.com>
2020-03-19 14:06:51 +0800

    more efficient rendering/update
    does not cause a rerender when there is no difference in fetching of data
    
    Look into websockets to improve update

--- app/components/custom/Logs.js
+++ app/components/custom/Logs.js
@@ -14,42 +14,45 @@ const useStyles = makeStyles(styles);
 
 const Logs = () => {
 	const classes = useStyles();
-	const [logs, setLogs] = useState([
-		{
-			_id: '5e71ebf8aa4f63b7a675bf27',
-			deviceName: 'postman',
-			type: 'checker',
-			datetime: '2020-03-18T09:38:00.834Z',
-			updatedAt: '2020-03-18T09:38:00.834Z',
-			__v: 0
-		}
-	]);
-	useEffect(() => {
-		async function fetchData() {
-			let response = await fetch('http://localhost:3001/api/log');
-			response = await response.json();
-			return response.data;
-		}
-		setInterval(() => {
-			fetchData().then(data => {
-				if (JSON.stringify(logs) !== JSON.stringify(data)) {
-					setLogs(data);
-				}
-			});
-		}, 3000);
-	}, []);
+
+	const [logs, setLogs] = useState([]);
+
+	const origin = typeof window !== 'undefined' ? window.location.origin : false;
+	if (origin) {
+		useEffect(() => {
+			async function fetchData() {
+				let response = await fetch(`${origin}/api/log`);
+				response = await response.json();
+				return response.data;
+			}
+
+			// Continous Running
+			const loop = () => {
+				console.log('FETI');
+				fetchData().then(data => {
+					if (JSON.stringify(logs) !== JSON.stringify(data)) {
+						setLogs(data);
+					}
+				});
+				window.setTimeout(loop, 3000);
+			};
+			loop();
+		}, []);
+	}
 	return (
 		<div className={classes.section}>
 			<GridContainer justify='center'>
 				<GridItem xs={12} sm={12} md={8}>
-					<h2 className={classes.title}>RFID Logs</h2>
-				</GridItem>
+					<h2 className={classes.title}> RFID Logs </h2>{' '}
+				</GridItem>{' '}
 				{logs.map(log => (
-					<GridItem xs={12} sm={12} md={8}>
-						<Log {...log} key={log._id}></Log>
+					<GridItem xs={12} sm={12} md={8} key={log._id}>
+						<Log {...log} key={log._id}>
+							{' '}
+						</Log>{' '}
 					</GridItem>
-				))}
-			</GridContainer>
+				))}{' '}
+			</GridContainer>{' '}
 		</div>
 	);
 };

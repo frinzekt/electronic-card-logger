c8b560b417b6300f74090205ebcb86447af5a315
frinzekt <frinzekt@gmail.com>
2020-06-19 23:52:20 +0800

    remove separate express server

--- app/server.js
+++ app/server.js
@@ -1,20 +1,51 @@
-const app = require('express')();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+// const app = require('express')();
+// const server = require('http').Server(app);
+// const io = require('socket.io')(server);
 
-// socket.io server
-io.on('connection', (socket) => {
-	socket.on('message', (data) => {
-		socket.broadcast.emit('message', data);
-		console.log('Message Sent', data);
-	});
-	socket.on('firstConnect', (data) => {
-		console.log('Client has connected');
-	});
-});
-app.get('/', (req, res) => res.send('Socket Server is Online'));
+// // socket.io server
+// io.on('connection', (socket) => {
+// 	socket.on('message', (data) => {
+// 		socket.broadcast.emit('message', data);
+// 		console.log('Message Sent', data);
+// 	});
+// 	socket.on('firstConnect', (data) => {
+// 		console.log('Client has connected');
+// 	});
+// });
+// app.get('/', (req, res) => res.send('Socket Server is Online'));
 
-server.listen(8000, (err) => {
-	if (err) throw err;
-	console.log('> Ready on http://localhost:8000');
+// server.listen(8000, (err) => {
+// 	if (err) throw err;
+// 	console.log('> Ready on http://localhost:8000');
+// });
+
+const next = require('next');
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
+const handler = app.getRequestHandler();
+
+const { createServer } = require('http');
+const { parse } = require('url');
+
+app.prepare().then(() => {
+	const server = createServer((req, res) => {
+		const parsedUrl = parse(req.url, true);
+		handler(req, res, parsedUrl);
+	});
+
+	server.listen(3000, (err) => {
+		if (err) throw err;
+		console.log('> Ready on http://localhost:3000');
+	});
+
+	const io = require('socket.io')(server);
+	// socket.io server
+	io.on('connection', (socket) => {
+		socket.on('message', (data) => {
+			socket.broadcast.emit('message', data);
+			console.log('Message Sent', data);
+		});
+		socket.on('firstConnect', (data) => {
+			console.log('Client has connected');
+		});
+	});
 });

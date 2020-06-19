fa8c2d52d76435723e6ca648902b7e1bf0fe2d3f
frinzekt <frinzekt@gmail.com>
2020-06-19 19:45:10 +0800

    alerts and new data polling

--- app/components/custom/Logs.js
+++ app/components/custom/Logs.js
@@ -5,18 +5,34 @@ import { makeStyles } from '@material-ui/core/styles';
 // core components
 import GridContainer from 'components/Grid/GridContainer.js';
 import GridItem from 'components/Grid/GridItem.js';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 import Log from './Log';
 
 import styles from 'assets/jss/nextjs-material-kit/pages/landingPageSections/productStyle.js';
-
 const useStyles = makeStyles(styles);
+
+const Alert = props => {
+	return <MuiAlert elevation={6} variant='filled' {...props} />;
+};
 
 const Logs = () => {
 	const classes = useStyles();
 
+	const [open, setOpen] = useState(false);
 	const [logs, setLogs] = useState([]);
 
+	// HANDLE DATA UPDATE NOTIFICATION
+
+	const handleClose = (event, reason) => {
+		if (reason === 'clickaway') {
+			return;
+		}
+		setOpen(false);
+	};
+
+	// DATA RENDERING
 	const origin = typeof window !== 'undefined' ? window.location.origin : false;
 	if (origin) {
 		useEffect(() => {
@@ -25,21 +41,26 @@ const Logs = () => {
 				response = await response.json();
 				return response.data;
 			}
-
-			// Continous Running
-			const loop = () => {
+			const timer = setTimeout(() => {
 				fetchData().then(data => {
 					if (JSON.stringify(logs) !== JSON.stringify(data)) {
 						setLogs(data);
 					}
 				});
-				window.setTimeout(loop, 3000);
+			}, 3000);
+			return () => {
+				clearTimeout(timer);
 			};
-			loop();
-		}, []);
+		}, [logs]);
 	}
 	return (
 		<div className={classes.section}>
+			{' '}
+			{/* <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
+							<Alert onClose={handleClose} severity='success'>
+								This is a success message!
+							</Alert>
+						</Snackbar> */}{' '}
 			<GridContainer justify='center'>
 				<GridItem xs={12} sm={12} md={8}>
 					<h2 className={classes.title}> RFID Logs </h2>{' '}

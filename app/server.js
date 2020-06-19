8fb128afd77f4131746ca747430fe14f3d9a0bfc
frinzekt <frinzekt@gmail.com>
2020-06-19 22:24:54 +0800

    express server

--- app/server.js
+++ app/server.js
@@ -1,32 +1,19 @@
-const express = require('express')
-const next = require('next')
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+// socket.io server
+io.on('connection', (socket) => {
+	socket.on('message', (data) => {
+		socket.broadcast.emit('message', data);
+		console.log('Message Sent', data);
+	});
+	socket.on('firstConnect', (data) => {
+		console.log('Client has connected');
+	});
+});
 
-app.prepare().then(() => {
-  const server = express()
-
-  server.get('/a', (req, res) => {
-    return app.render(req, res, '/a', req.query)
-  })
-
-  server.get('/b', (req, res) => {
-    return app.render(req, res, '/b', req.query)
-  })
-
-  server.get('/posts/:id', (req, res) => {
-    return app.render(req, res, '/posts', { id: req.params.id })
-  })
-
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
-
-  server.listen(port, err => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+server.listen(8000, (err) => {
+	if (err) throw err;
+	console.log('> Ready on http://localhost:8000');
+});

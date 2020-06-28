078755c13be003f3e50ef54122b6c740caee0a11
Frinze Erin Lapuz <44391389+frinzekt@users.noreply.github.com>
2020-06-28 23:03:54 +0800

    reset connection every send

--- app/pages/api/log.js
+++ app/pages/api/log.js
@@ -3,10 +3,6 @@ import { getLogs, addLog } from '../../controller/log';
 import io from 'socket.io-client';
 // import mail from '../../controller/nodemailer';
 
-// SOCKET EMITTER
-const socketURL = process.env.socketURL;
-const socket = io(`${socketURL}`);
-
 export default async (req, res) => {
 	// Get data from your database
 	if (req.method === 'GET') {
@@ -20,6 +16,9 @@ export default async (req, res) => {
 	} else if (req.method === 'POST') {
 		try {
 			const result = await addLog(req.body);
+			// SOCKET EMITTER
+			const socketURL = process.env.socketURL;
+			const socket = io(`${socketURL}`);
 			socket.emit('message', result);
 			return res.status(200).json({ result });
 		} catch (err) {
